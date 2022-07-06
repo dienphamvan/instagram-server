65a67af9d83b28aabaf91fc87f11ba240b9ee554
zinzin <chuanthanh133@gmail.com>
2022-07-06 15:00:22 +0700

    Change Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/posts", async (req, res) => {
 
 app.post("/users", async (req, res) => {
   const data = await postNewUser(req.headers);
-  return res.send(data);
+  return res.set('Access-Control-Allow-Origin', '*').send(data);
 });
 
 app.post("/posts", async (req, res) => {
